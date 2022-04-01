76f428bc47e719a4a812ccd927bac27338897a50
Yubo Wang <yubowang2007@gmail.com>
2022-04-01 19:55:33 +0100

    still

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,9 @@ const config = {
  // Consult https://github.com/sveltejs/svelte-preprocess
  // for more information about preprocessors
  preprocess: preprocess(),
-
+ prerender : { 
+   default: true
+  },
  kit: {
  // hydrate the <div id="svelte"> element in src/app.html
   adapter: adapter({
